74576b7eeec0d4952addb1f8be62a6617cdee8f2
SHAN-SN <85439835+SHAN-SN@users.noreply.github.com>
2022-06-27 11:27:13 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -452,17 +452,14 @@ async def cb_handler(client: Client, query: CallbackQuery):
         await query.answer()
     elif query.data == "start":
         buttons = [[
-            InlineKeyboardButton('➕ Add me to your Chat ➕', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
+            InlineKeyboardButton('⌨ 𝐀𝐋𝐋 𝐌𝐎𝐕𝐈𝐄𝐒 𝐇𝐔𝐁 ⌨', url='https://t.me/MHChats')
             ],[
-            InlineKeyboardButton('⭕️ Help', callback_data='help'),
-            InlineKeyboardButton('About ⭕️', callback_data='about')
+            InlineKeyboardButton('💺 𝐍𝐞𝐰 𝐌𝐨𝐯𝐢𝐞𝐬 🍿', url='https://t.me/MovieHub_OTT'),
+            InlineKeyboardButton('𝐂𝐡𝐚𝐧𝐧𝐞𝐥𝐬 📋', url='https://t.me/MH_Linkz')
             ],[
-            InlineKeyboardButton('🕵️ Search here Movie 🕵️', switch_inline_query_current_chat='')
-            ],[
-            InlineKeyboardButton('⭕️ Updates', url='https://t.me/josprojects'),
-            InlineKeyboardButton('Movie Club ⭕️', url='https://t.me/+y53tWFUw6Q43NzE9')
-            ],[
-            InlineKeyboardButton('❌ Close the Menu ❌', callback_data='close_data')
+            InlineKeyboardButton('🗣 𝐃𝐢𝐬𝐜𝐮𝐬𝐬 𝐆𝐫𝐩', url='https://t.me/MoviesHub_Discuss'),
+            InlineKeyboardButton('🧩 𝐍𝐞𝐰 𝐋𝐢𝐧𝐤𝐬', url='https://t.me/MH_Linkz'),
+            InlineKeyboardButton('📨 𝘕𝘦𝘸 𝘜𝘱𝘥𝘢𝘵𝘦𝘴', url='https://t.me/MoviesHub_Updates')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
